465eb0d28318bc845cc125692ea1ac9d4e491aee
Thinakaran <thinakaranmanohar@yahoo.com>
2025-04-17 13:46:10 +0530

    sdfsfsf

---
+++ DnA/index.js
@@ -0,0 +1,55 @@
+import System from './home/view-3d-laptop-device-with-screen-keyboard.jpg'
+import FeatureBG from './home/pexels-lenin-estrada-117221-2569997.jpg'
+import Quality from './home/close-up-woman-typing-keyboard.jpg'
+import Offer from './home/offer.jpg'
+import Prices from './home/prices.jpg'
+import Support from './home/support.jpg'
+import Team from './home/teammang.jpg'
+import Tech from './home/technologies.jpg'
+import Time from './home/time.jpg'
+
+import Man from './home/mann.jpg'
+import Skull from './home/skull.jpg'
+
+import Proj1 from './home/projects/InShot_20250120_142422269.jpg'
+import Proj2 from './home/projects/InShot_20250120_142543564.jpg'
+import Proj3 from './home/projects/InShot_20250120_142617010.jpg'
+import Proj4 from './home/projects/InShot_20250120_142849425.jpg'
+import Proj5 from './home/projects/InShot_20250120_142919651.jpg'
+
+import AboutBG from './about/office.jpg'
+import Mission from './about/mission.jpg'
+import Vision from './about/vision.jpg'
+import Card1 from './about/card1.jpg'
+import Card2 from './about/techno.jpg'
+import Card3 from './about/suppoert.jpg'
+import Card4 from './about/delievr.jpg'
+import Card5 from './about/compromissing.jpg'
+import Card6 from './about/teach.jpg'
+
+import ServicesBG from './services/servicebg.jpg'
+import SerCard1 from './services/thumbsup.jpg'
+import SerCard2 from './services/custom.jpg'
+import SerCard3 from './services/mentor.jpg'
+import SerCard4 from './services/docs.jpg'
+import SerCard5 from './services/chno.jpg'
+import SerCard6 from './services/ltsupport.jpg'
+
+import ContactBG from './contact/copntactbg.jpg'
+
+import Dessert from './account/dessert.jpg'
+import Retro from './account/2615525.jpg'
+
+export default {
+    System, FeatureBG, Quality, Offer, Prices, Quality, Team, Tech, Support, Time,
+    Man, Skull,  
+    Proj1, Proj2, Proj3, Proj4, Proj5,
+
+    AboutBG, Mission, Vision, Card1, Card2, Card3, Card4, Card5, Card6,
+
+    ServicesBG, SerCard1, SerCard2, SerCard3, SerCard4, SerCard5, SerCard6,
+
+    ContactBG,
+
+    Dessert, Retro,
+}
