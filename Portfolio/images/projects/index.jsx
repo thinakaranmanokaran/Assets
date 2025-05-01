dc596be85c0f480891b9b62e10baa31a07383680
Thinakaran <thinakaranmanohar@yahoo.com>
2025-05-01 20:25:06 +0530

    Portfolio Assets added

---
+++ Portfolio/images/projects/index.jsx
@@ -0,0 +1,15 @@
+import BGRemover from './laptop-mockup-near-glass-object_23-2151832845.jpg'
+import PackageManager from './snappm2.png'
+import SpotifyDownloader from './36078302_8372726.jpg'
+import ProjectHub from './projhub.png'
+import Nucaza from './homepage-seen-computer-screen.jpg'
+import VKPStudio from './20548064_6317757.jpg'
+import GameSite from './game.jpg'
+
+
+export {
+    BGRemover,
+    PackageManager,
+    SpotifyDownloader,
+    ProjectHub, Nucaza, VKPStudio, GameSite
+}
